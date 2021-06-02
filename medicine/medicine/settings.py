6fa6761659a2b1581976f3507b31f13a72938f50
SeungJoon Lee <68410822+joon2301@users.noreply.github.com>
2021-06-02 21:58:02 +0900

    Update settings.py

--- medicine/medicine/settings.py
+++ medicine/medicine/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'NAME': 'mydb',
         'USER': 'root',
         'PASSWORD': '1234',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
 }
